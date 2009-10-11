281d81c4a8c046f11b3ab8bb7f88d9677695cda3
Maciej Litwiniuk <maciej@galdomedia.pl>
2009-10-11 17:34:48 +0200

    fixed migration

--- db/migrate/20091011130003_create_roles_in_system.rb
+++ db/migrate/20091011130003_create_roles_in_system.rb
@@ -3,9 +3,6 @@ class CreateRolesInSystem < ActiveRecord::Migration
     change_table :users do |t|
       t.boolean :is_staff, :default=>false
     end
-    create_table :roles do |t|
-      t.string :name
-    end
 
     create_table :roles_users, :id=>false do |t|
       t.references :role
@@ -15,7 +12,6 @@ class CreateRolesInSystem < ActiveRecord::Migration
 
   def self.down
     drop_table :roles_users
-    drop_table :roles
     remove_column :users, :is_staff
   end
 end
