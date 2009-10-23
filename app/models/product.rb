49f6d9b06acbd4114ff0cf47544ab7dee47d1dfe
Maciej Litwiniuk <maciej@galdomedia.pl>
2009-10-23 13:45:32 +0200

    fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -57,12 +57,36 @@ class Product < ActiveRecord::Base
   end
   alias_method :get_price, :get_minimal_price
 
+  def get_first_variation
+    if self.product_variations.count > 0
+      self.product_variations.first
+    else
+      nil
+    end
+  end
+  
   def get_maximal_price
     if self.product_variations.count > 0
       prices = self.product_variations.map{|pv| pv.price}
       prices.max
     else
       self.price
+    end
+  end
+  
+  def get_minimal_variation
+    if self.product_variations.count > 0
+      self.product_variations.min{|a,b| a.price<=>b.price}
+    else
+      nil
+    end
+  end
+
+  def get_maximal_variation
+    if self.product_variations.count > 0
+      self.product_variations.max{|a,b| a.price<=>b.price}
+    else
+      nil
     end
   end
 
--- app/models/product_variation.rb
+++ app/models/product_variation.rb
@@ -2,6 +2,7 @@ class ProductVariation < ActiveRecord::Base
 
   has_and_belongs_to_many :options
   belongs_to :product
+  belongs_to :option
 
 
   validates_numericality_of :price, :only_integer=>false, :greater_than_or_equal_to=>0, :allow_nil=>false
@@ -9,6 +10,10 @@ class ProductVariation < ActiveRecord::Base
   def to_s
     s = self.options.map{|o| "#{o.option_group.name}: #{o.name}"}
     s.join ", "
+  end
+  
+  def options_code_str
+    self.options.map{|o| "#{o.code}"}.join ", "
   end
 
 end
