c27b5750e456f5661f766fa752ccd2167b3e8167
Maciej Litwiniuk <maciej@galdomedia.pl>
2009-10-24 17:38:15 +0200

    fixed attr_accessible in contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < ActiveRecord::Base
   validates_presence_of :is_billing, :if=>proc{|c| c['is_shipping'].blank? or c['is_shipping']==false}
   validates_presence_of :is_shipping, :if=>proc{|c| c['is_billing'].blank? or c['is_billing']==false}
 
-  attr_accessible :first_name, :last_name, :street, :street_nr, :city, :country, :is_billing, :is_shipping, :zip_code, :email, :phone
+  attr_accessible :first_name, :last_name, :street, :street_nr, :city, :country, :is_billing, :is_shipping, :zip_code, :email, :phone, :is_company, :company_name, :vat_code
   
   def name
     "#{self.first_name} #{self.last_name}"
